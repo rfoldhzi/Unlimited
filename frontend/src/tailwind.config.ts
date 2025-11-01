af440acba1aae311eaf34e1e36a9be77996983f3
reeganR1 <89109947+rfoldhzi@users.noreply.github.com>
2025-10-31 23:30:06 -0500

    Install tailwind
    Right now used for card modal

---
+++ frontend/src/tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan all JS/JSX/TS/TSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
